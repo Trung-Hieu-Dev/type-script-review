f313297ea04b60141a8fc2aec3f8493e41090c3e
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-05 20:29:59 +0700

    Basic: Create First Typescript App

---
+++ 01-basic/01-helloWorld/helloWorld.ts
@@ -0,0 +1,8 @@
+/**
+ * How to run typescript file
+ * Step 1: Create helloWorld.ts file
+ * Step 2: Generate .js file    -> tsc helloWorld.ts
+ * Step : Run .js file          -> node helloWorld.js 
+ */
+
+console.log("Hello World");
